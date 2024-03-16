799f5f6bf9582aa40d9b2a27a7ec8fb645fd3636
Sanket <patilsanket792@gmail.com>
2024-03-16 15:54:28 +0530

    Day 1

---
+++ Day1.java
@@ -0,0 +1,14 @@
+package java100DaysOfCoding;
+
+
+// Day 1 - Coding Statement- 
+// Write a program to Print  "Welcome to 100 Days of Java Coding Challenge" on the o/p Screen
+public class Day1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to 100 Days of Java Coding Challenge");
+		
+	}
+
+}
