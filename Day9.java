272f4e8c3220cb4f4699bf37d38752f4eace400f
Sanket <patilsanket792@gmail.com>
2024-03-24 19:29:13 +0530

    Day9

---
+++ Day9.java
@@ -0,0 +1,26 @@
+package java100DaysOfCoding;
+
+public class Day9 {
+
+	public static void main(String[] args) {
+		int a=50;
+		int b=100;
+		int c=20;
+		
+		if(a>b && a>c)
+		{
+			System.out.println(" a is greatest");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is largest");
+		}
+		else
+		{
+			System.out.println("c is greatest");
+		}
+		
+
+	}
+
+}
